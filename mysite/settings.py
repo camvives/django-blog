34fdf01c82c53ca72734dcbb1b35f3863ba7aea9
camvives <camvives@gmail.com>
2023-05-17 11:47:56 -0300

    vistas post

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@m#^hlc1@y(f5ht^5q!+mclviflbhbcei8*63ey=*0=rc@&n*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
