341c38f033ea81695c838140ddd95c08f8fcb61a
camvives <camvives@gmail.com>
2023-05-17 12:45:03 -0300

    vistas crear/editar posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@m#^hlc1@y(f5ht^5q!+mclviflbhbcei8*63ey=*0=rc@&n*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
